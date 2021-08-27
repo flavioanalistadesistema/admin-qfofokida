c03ceb1dc25591d51206ecfc99bf899a75195dc5
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-08-27 08:28:25 -0300

    chorce: Config docker-compose

--- docker-compose/mysql/init_db.sql
+++ docker-compose/mysql/init_db.sql
@@ -1,12 +1,10 @@
 DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users`(
-`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-`name` varchar(30) NOT NULL,
-`email` varchar(50),
-`password` VARCHAR (100)
-PRIMARY KEY (codigo)
-)ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
-INSERT INTO `users` (name, email, password)
-VALUES ('flaaps', 'flavioanalistadesistema@gmail.com', 123456);
+CREATE TABLE `users` (
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email` varchar(255) NOT NULL DEFAULT '0',
+    `password` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+INSERT INTO `users` (name, visited)
+VALUES ('Flavio', 'flavioanalistadesistema@gmail.com', 123456);
